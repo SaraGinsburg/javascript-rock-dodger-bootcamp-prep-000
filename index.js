bacfd0b4bcc2382673c03452a6cf252a1cc40cef
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2018-02-05 17:27:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ function checkCollision(rock) {
 
     if (( rockLeftEdge < dodgerLeftEdge && rockRightEdge > dodgerLeftEdge) ||
       (rockLeftEdge > dodgerLeftEdge  && rockRightEdge < dodgerRightEdge) ||
-      (rockLeftEdge < dodgerRightEdge)){
+      (rockLeftEdge < dodgerRightEdge && rockRightEdge > dodgerRightEdge)){
         return true
     } else {
       return false
